19865528e3e2c668f1148feb7ba9d3013ecab15a
thunder <cyserser@gmail.com>
2020-11-09 15:56:18 +0000

    Initial commit

---
+++ app/Http/Controllers/problemasT2Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class problemasT2Controller extends Controller
+{
+   public function procesarRegistro() {
+        return view('procesarRegistro');
+    }
+}
---
+++ resources/views/formulario1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+
+<form id="form1" name="form1" method="get" action="procesarRegistro">
+
+   <script language="JavaScript" src="formulario1.js"></script>
+
+    <p><label>Nombre</label> <input type="text" size="15" name="nombre"></p>
+
+    <p><label>Fecha de nacimiento</label> <input id="date" type="date" size="15" name="date" value="2000-10-22"></p>
+
+    <p><label>Email</label> <input type="email" size="15" name="email"></p>
+
+    <p><label>Contraseña</label> <input type="password" value="12345678" size="15" name="password" minlength="8" maxlength="14" pattern="[A-Za-z0-9]+" required></p> <!-- el + para se pueda introducir todos los caracteres-->
+
+    <p><label>Repita contraseña</label> <input type="password" value="12345678" size="15" name="password2"></p>
+
+    <input type="submit" value="Enviar"> <input type="reset" value="Borrar">
+
+
+</form>
+</body>
+</html>
